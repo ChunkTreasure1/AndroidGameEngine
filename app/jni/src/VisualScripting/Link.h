bf4d42588f2ac37a6c6c0ab1103ef61a065f7136
Ivar Jönsson <chunktreasure@gmail.com>
2021-08-02 12:18:00 +0200

    Created base for visual scripting

---
+++ app/jni/src/VisualScripting/Link.h
@@ -0,0 +1,8 @@
+//
+// Created by chunktreasure on 8/2/21.
+//
+
+#ifndef TESTPROJECT_LINK_H
+#define TESTPROJECT_LINK_H
+
+#endif //TESTPROJECT_LINK_H
--- app/jni/src/VisualScripting/Nodes/IfNode.cpp
+++
@@ -1,5 +0,0 @@
-//
-// Created by chunktreasure on 7/26/21.
-//
-
-#include "IfNode.h"
